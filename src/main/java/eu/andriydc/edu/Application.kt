b207e193daaa7eaeda8d79d70a4c8939c6717d1e
Andriy G <andriyg@icloud.com>
2016-03-29 00:02:56 +0300

    Extract separate service

--- src/main/java/eu/andriydc/edu/Application.kt
+++ src/main/java/eu/andriydc/edu/Application.kt
@@ -1,7 +1,5 @@
 package eu.andriydc.edu
 
-import eu.andriydc.edu.dal.Customer
-import eu.andriydc.edu.dal.CustomerRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
@@ -9,35 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
 open class Application : CommandLineRunner {
-    @Autowired var repository: CustomerRepository? = null
+    @Autowired var command: CustomerCommand? = null
 
     override fun run(vararg args: String) {
 
-        val repository = repository!!
-        repository.deleteAll()
-
-        // save a couple of customers
-        repository.save(Customer("Alice", "Smith"))
-        repository.save(Customer("Bob", "Smith"))
-
-        // fetch all customers
-        println("Customers found with findAll():")
-        println("-------------------------------")
-        for (customer in repository.findAll()) {
-            println(customer)
-        }
-        println()
-
-        // fetch an individual customer
-        println("Customer found with findByFirstName('Alice'):")
-        println("--------------------------------")
-        println(repository.findByFirstName("Alice"))
-
-        println("Customers found with findByLastName('Smith'):")
-        println("--------------------------------")
-        for (customer in repository.findByLastName("Smith")) {
-            println(customer)
-        }
+        command!!.execute()
 
     }
 
